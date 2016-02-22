f1097f32e21c8635d4a8a5e347633149401db429
Jannik Nilsson <jan@pentia.dk>
2016-02-22 17:56:31 +0100

    Added atom

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,11 @@
 #Set-ExecutionPolicy bypass
+#
+# Set the $HOME variable for our use
+# and make powershell recognize ~\ as $HOME
+# in paths
+#
+set-variable -name HOME -value (resolve-path $env:Home) -force
+(get-psprovider FileSystem).Home = $HOME
 
 $psdir=$env:USERPROFILE
 
@@ -11,7 +18,36 @@ Set-Alias cd CdDash
 
 Set-Alias sub "C:\Program Files\Sublime Text 3\sublime_text.exe"
 
+
+
+function atom-run([string] $arg) { & $($env:LOCALAPPDATA+ "\atom\bin\atom.cmd") $arg}
+Set-Alias -Name atom -Value atom-run
+
+
+
 Set-Alias android AndroidDebug
 
 cd c:\
 
+function shorten-path([string] $path) { 
+   $loc = $path.Replace($HOME, '~') 
+   # remove prefix for UNC paths 
+   $loc = $loc -replace '^[^:]+::', '' 
+   # make path shorter like tabs in Vim, 
+   # handle paths starting with \\ and . correctly 
+   return ($loc -replace '\\(\.?)([^\\])[^\\]*(?=\\)','\$1$2') 
+}
+
+function prompt { 
+   # our theme 
+   $cdelim = [ConsoleColor]::DarkCyan 
+   $chost = [ConsoleColor]::Green 
+   $cloc = [ConsoleColor]::Cyan 
+
+   write-host "$([char]0x0A7) " -n -f $cloc 
+   write-host ([net.dns]::GetHostName()) -n -f $chost 
+   write-host ' {' -n -f $cdelim 
+   write-host (shorten-path (pwd).Path) -n -f $cloc 
+   write-host '}' -n -f $cdelim 
+   return ' ' 
+}
