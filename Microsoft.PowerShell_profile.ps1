c3064881ff552cdcc82122b991ffe2e096ace39c
Jannik Nilsson <jan@pentia.dk>
2016-02-22 18:00:55 +0100

    Merged shortend prompt into posh git

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,8 +4,7 @@
 # and make powershell recognize ~\ as $HOME
 # in paths
 #
-set-variable -name HOME -value (resolve-path $env:Home) -force
-(get-psprovider FileSystem).Home = $HOME
+
 
 $psdir=$env:USERPROFILE
 
@@ -28,26 +27,3 @@ Set-Alias -Name atom -Value atom-run
 Set-Alias android AndroidDebug
 
 cd c:\
-
-function shorten-path([string] $path) { 
-   $loc = $path.Replace($HOME, '~') 
-   # remove prefix for UNC paths 
-   $loc = $loc -replace '^[^:]+::', '' 
-   # make path shorter like tabs in Vim, 
-   # handle paths starting with \\ and . correctly 
-   return ($loc -replace '\\(\.?)([^\\])[^\\]*(?=\\)','\$1$2') 
-}
-
-function prompt { 
-   # our theme 
-   $cdelim = [ConsoleColor]::DarkCyan 
-   $chost = [ConsoleColor]::Green 
-   $cloc = [ConsoleColor]::Cyan 
-
-   write-host "$([char]0x0A7) " -n -f $cloc 
-   write-host ([net.dns]::GetHostName()) -n -f $chost 
-   write-host ' {' -n -f $cdelim 
-   write-host (shorten-path (pwd).Path) -n -f $cloc 
-   write-host '}' -n -f $cdelim 
-   return ' ' 
-}
