f7b5c8e223e034ccbf247e67ee711f609b4a85c8
Jannik Nilsson <janniknilsson@outlook.com>
2016-02-26 22:11:59 +0100

    Removed the host name from the path since it took up too much space

--- Modules/posh-git/profile.example.ps1
+++ Modules/posh-git/profile.example.ps1
@@ -13,30 +13,31 @@ Import-Module .\posh-git
 set-variable -name HOME -value (resolve-path $env:Home) -force
 (get-psprovider FileSystem).Home = $HOME
 
-function shorten-path([string] $path) { 
-   $loc = $path.Replace($HOME, '~') 
-   # remove prefix for UNC paths 
-   $loc = $loc -replace '^[^:]+::', '' 
-   # make path shorter like tabs in Vim, 
-   # handle paths starting with \\ and . correctly 
-   return ($loc -replace '\\(\.?)([^\\])[^\\]*(?=\\)','\$1$2') 
+function shorten-path([string] $path) {
+   $loc = $path.Replace($HOME, '~')
+   # remove prefix for UNC paths
+   $loc = $loc -replace '^[^:]+::', ''
+   # make path shorter like tabs in Vim,
+   # handle paths starting with \\ and . correctly
+   return ($loc -replace '\\(\.?)([^\\])[^\\]*(?=\\)','\$1$2')
 }
 
-function prompt { 
-   # our theme 
-   $cdelim = [ConsoleColor]::DarkCyan 
-   $chost = [ConsoleColor]::Green 
-   $cloc = [ConsoleColor]::Cyan 
+function prompt {
+   # our theme
+   $cdelim = [ConsoleColor]::DarkCyan
+   $chost = [ConsoleColor]::Green
+   $cloc = [ConsoleColor]::Cyan
    $realLASTEXITCODE = $LASTEXITCODE
 
-   write-host "$([char]0x0A7) " -n -f $cloc 
-   write-host ([net.dns]::GetHostName()) -n -f $chost 
-   write-host ' {' -n -f $cdelim 
-   write-host (shorten-path (pwd).Path) -n -f $cloc 
-   write-host '}' -n -f $cdelim 
+   write-host "$([char]0x0A7)" -n -f $cloc
+   # write-host ([net.dns]::GetHostName()) -n -f $chost
+   # write-host "$([char]0x07E) " -n -f $chost
+   write-host ' {' -n -f $cdelim
+   write-host (shorten-path (pwd).Path) -n -f $cloc
+   write-host '}' -n -f $cdelim
    Write-VcsStatus
    $global:LASTEXITCODE = $realLASTEXITCODE
-   return '> ' 
+   return '> '
 }
 
 #Pop-Location
